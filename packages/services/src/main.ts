b70978f382f554e2ff804b8af68c1e346c3cbcba
alirezahematidev <alirezahematideveloper71@gmail.com>
2025-02-04 00:37:04 +0330

    feat: request builder

--- packages/services/src/main.ts
+++ packages/services/src/main.ts
@@ -1,24 +1,84 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type TransformEndpoint<T extends string[]> = T extends [
+  infer R extends string,
+  ...infer Rest extends string[],
+]
+  ? `/${R}${Rest['length'] extends 0 ? '' : TransformEndpoint<Rest>}`
+  : '';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface ServiceBuilderOptions<Endpoint extends string> {
+  endpoint: Endpoint;
+}
+
+const BASE_URL = 'http://localhost:8000';
+
+type RequestData = {
+  queryParams?: Record<string, string>;
+};
+
+function createUseQuery<Endpoint extends string[], Response>({
+  endpoint,
+}: ServiceBuilderOptions<TransformEndpoint<Endpoint>>) {
+  return (requestData?: RequestData) => {
+    const { data, error, isLoading } = useQuery<[Endpoint, RequestData], Error, Response>({
+      queryKey: [endpoint, requestData],
+      queryFn: async ({ signal }) => {
+        const response = await fetch(BASE_URL + endpoint, {
+          method: 'GET',
+          signal,
+        });
+
+        if (!response.ok) throw new Error('error');
+
+        const data = await response.json();
+
+        return data;
+      },
+    });
+
+    return { data, error, isLoading };
+  };
+}
+
+type MutationCallbacks<Request, Response = void> = {
+  onSuccess?: (data: Response, payload: Request) => Promise<void>;
+  onError?: (error: Error, payload: Request) => Promise<void>;
+  onSettled?: (data: Response | undefined, error: Error | null, payload: Request) => void;
+};
+
+function createUseMutation<Endpoint extends string[], Request, Response = void>({
+  endpoint,
+}: ServiceBuilderOptions<TransformEndpoint<Endpoint>>) {
+  return (callbacks: MutationCallbacks<Request, Response>, requestData?: RequestData) => {
+    const { mutate, data, error, isPending } = useMutation<Response, Error, Request>({
+      mutationKey: [endpoint],
+      mutationFn: async (payload) => {
+        const response = await fetch(BASE_URL + endpoint, {
+          method: 'POST',
+          body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) throw new Error(await response.json());
+
+        const data = await response.json();
+
+        return data;
+      },
+      onSuccess(data, payload) {
+        if (callbacks.onSuccess) callbacks.onSuccess(data, payload);
+      },
+      onError(error, payload) {
+        if (callbacks.onError) callbacks.onError(error, payload);
+      },
+      onSettled(data, error, payload) {
+        if (callbacks.onSettled) callbacks.onSettled(data, error, payload);
+      },
+      scope: { id: endpoint },
+    });
+
+    return { mutate, data, error, isLoading: isPending };
+  };
+}
+
+export { createUseQuery, createUseMutation };
