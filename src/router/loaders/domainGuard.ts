894c2fa237d8758170086a241a75e98d3733805c
hamidreza.gh <hamidrezag.gh@smartech.ir>
2025-06-03 12:32:18 +0330

    chore: add some changes

--- src/router/loaders/domainGuard.ts
+++ src/router/loaders/domainGuard.ts
@@ -3,8 +3,8 @@ import { LoaderFunction, redirect } from 'react-router-dom';
 import { useDomainStore } from '@/store';
 
 const domainGuardLoader: LoaderFunction = async ({ request }) => {
-  const { domain } = useDomainStore.getState();
-  const url = new URL(request.url);
+  // const { domain } = useDomainStore.getState();
+  // const url = new URL(request.url);
 
   // If no domain is set and user is not trying to access products page, redirect to products
   // if (!domain && !url.pathname.startsWith('/products')) {
